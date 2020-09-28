a8436e263cc9640ec2002efce3f7b3b64be8fc8a
NicolaiOkkels <nico902@stud.kea.dk>
2020-09-28 15:43:08 +0200

    added GetMapping + input box for forbruger

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -1,4 +1,33 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyController {
+
+    @GetMapping("/")
+    public String homepage(){
+        return "homepage";
+    }
+
+    @GetMapping("/forbruger")
+    public String user(){
+        return "forbruger";
+    }
+
+    @GetMapping("/kontakt")
+    public String contact(){
+        return "kontakt";
+    }
+
+    @GetMapping("/læs mere")
+    public String readMore(){
+        return "læs-mere";
+    }
+
+    @GetMapping("/samarbejdspartner")
+    public String partner(){
+        return "samarbejdspartner";
+    }
 }
