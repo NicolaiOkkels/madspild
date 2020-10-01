c374824d924048ee04c412e4d8f9e00662463808
NicolaiOkkels <nico902t@stud.kea.dk>
2020-10-01 14:43:02 +0200

    added reserve and buy button to product list

--- src/main/java/com/example/demo/model/Cities.java
+++ src/main/java/com/example/demo/model/Cities.java
@@ -24,35 +24,52 @@ public class Cities {
             product1.setProductName("Æble");
             product1.setProductPrice(10);
             product1.setDiscount(75);
-            product1.setExpireDate(LocalDate.of(2020, 9, 26));
+            product1.setExpireDate(LocalDate.of(2020, 10, 1));
+            product1.setReserved(false);
+
+            Product product2 = new Product();
+            product2.setProductName("Mælk");
+            product2.setProductPrice(20);
+            product2.setDiscount(50);
+            product2.setExpireDate(LocalDate.of(2020, 10, 4));
+            product2.setReserved(true);
+
+            Product product3 = new Product();
+            product3.setProductName("Jordbær");
+            product3.setProductPrice(10);
+            product3.setDiscount(60);
+            product3.setExpireDate(LocalDate.of(2020, 10, 2));
+            product3.setReserved(true);
 
             store1.createProduct(product1);
+            store1.createProduct(product2);
+            store1.createProduct(product3);
 
             Store store2 = new Store();
             store2.setStoreName("Brugsen");
             store2.setAdresse("Brugsen addresse");
             city1.createStore(store2);
 
-            Product product2 = new Product();
-            product2.setProductName("Banan");
-            product2.setProductPrice(12);
-            product2.setDiscount(75);
-            product2.setExpireDate(LocalDate.of(2020, 9, 30));
+            Product product4 = new Product();
+            product4.setProductName("Banan");
+            product4.setProductPrice(12);
+            product4.setDiscount(75);
+            product4.setExpireDate(LocalDate.of(2020, 9, 30));
 
-            store2.createProduct(product2);
+            store2.createProduct(product4);
 
             Store store3 = new Store();
             store3.setStoreName("Spar");
             store3.setAdresse("Spar addresse");
             city1.createStore(store3);
 
-            Product product3 = new Product();
-            product3.setProductName("Pære");
-            product3.setProductPrice(12);
-            product3.setDiscount(75);
-            product3.setExpireDate(LocalDate.of(2020, 10, 1));
+            Product product5 = new Product();
+            product5.setProductName("Pære");
+            product5.setProductPrice(12);
+            product5.setDiscount(75);
+            product5.setExpireDate(LocalDate.of(2020, 10, 1));
 
-            store3.createProduct(product3);
+            store3.createProduct(product5);
 
             //------------------------------------------------------------------------------------------//
             City city2 = new City();
@@ -65,26 +82,26 @@ public class Cities {
             store4.setAdresse("Købmand addresse");
             city2.createStore(store4);
 
-            Product product4 = new Product();
-            product4.setProductName("Brød");
-            product4.setProductPrice(20);
-            product4.setDiscount(75);
-            product4.setExpireDate(LocalDate.of(2020, 9, 26));
+            Product product6 = new Product();
+            product6.setProductName("Brød");
+            product6.setProductPrice(20);
+            product6.setDiscount(75);
+            product6.setExpireDate(LocalDate.of(2020, 9, 26));
 
-            store4.createProduct(product4);
+            store4.createProduct(product6);
 
             Store store5 = new Store();
             store5.setStoreName("Menu");
             store5.setAdresse("Menu addresse");
             city2.createStore(store5);
 
-            Product product5 = new Product();
-            product5.setProductName("Grøntsagskasse");
-            product5.setProductPrice(100);
-            product5.setDiscount(75);
-            product5.setExpireDate(LocalDate.of(2020, 10, 2));
+            Product product7 = new Product();
+            product7.setProductName("Grøntsagskasse");
+            product7.setProductPrice(100);
+            product7.setDiscount(75);
+            product7.setExpireDate(LocalDate.of(2020, 10, 2));
 
-            store5.createProduct(product5);
+            store5.createProduct(product7);
         }
 
         return cities;
