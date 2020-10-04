9c97c5cc8dcb60a92f23a77a61eb83a531dd2cb6
Maja <maja669e@stud.kea.dk>
2020-10-04 17:56:43 +0200

    added help button, fixed problem with burgermenu needed two clicks before and made navigation position:fixed in mobile size too

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,27 +1,50 @@
-window.addEventListener("load", sidenVises);
-
+document.addEventListener("DOMContentLoaded", burgerMenu);
 var burger = document.getElementById("myTopnav");
-var timer = 0;
-var i;
-var current, next;
-var moveSlideAnimClass = {
-    forCurrent: "",
-    forNext: ""
-}
-var slideIndex, slides, captionText;
 
-function sidenVises() {
-    console.log("sidenVises");
-}
+var slideIndex, slides, dot, captionText;
 
 function burgerMenu() {
+
 
     if (burger.className === "topnav") {
         burger.className += " responsive";
     } else {
         burger.className = "topnav";
     }
+
+    hjælp();
+    initGallery();
 }
+
+
+function hjælp() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("button4");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
 
 function initGallery() {
     slideIndex = 0;
@@ -30,6 +53,19 @@ function initGallery() {
 
     captionText = document.querySelector(".captionTextHolder .captionText");
     captionText.innerText = slides[slideIndex].querySelector(".captionText").innerText;
+
+    //add dots
+    dots = [];
+    var dotsContainer = document.getElementById("dotsContainer"),
+        i;
+    for (i = 0; i < slides.length; i++) {
+        var dot = document.createElement("span");
+        dot.classList.add("dots");
+        dotsContainer.append(dot);
+        dot.setAttribute("onclick", "moveSlide(" + i + ")");
+        dots.push(dot);
+    }
+    dots[slideIndex].classList.add("active");
 }
 initGallery();
 
@@ -38,7 +74,13 @@ function plusSlides(n) {
 }
 
 function moveSlide(n) {
-
+    var i;
+    var current, next;
+    var moveSlideAnimClass = {
+        forCurrent: "",
+        forNext: ""
+    };
+    var slideTextAnimClass;
     if (n > slideIndex) {
         if (n >= slides.length) {
             n = 0;
@@ -61,19 +103,20 @@ function moveSlide(n) {
         for (i = 0; i < slides.length; i++) {
             slides[i].className = "imageHolder";
             slides[i].style.opacity = 0;
+            dots[i].classList.remove("active");
         }
         current.classList.add(moveSlideAnimClass.forCurrent);
         next.classList.add(moveSlideAnimClass.forNext);
-
+        dots[n].classList.add("active");
         slideIndex = n;
     }
 
 }
-
+var timer = null;
 
 function setTimer() {
     timer = setInterval(function () {
         plusSlides(1);
-    }, 5000);
+    }, 4500);
 }
 setTimer();
